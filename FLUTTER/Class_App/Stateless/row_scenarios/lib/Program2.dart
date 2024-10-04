7e527f0594a30d0e3baba566e5bc39285b93c9cf
pallavikakhandaki <pallavi.kakhandaki@gmail.com>
2024-10-04 08:30:37 +0530

    Row Scenarios

---
+++ FLUTTER/Class_App/Stateless/row_scenarios/lib/Program2.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+class Program2 extends StatelessWidget{
+  const Program2({super.key});
+
+  @override  
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:const  Text(
+          "Row Scenarios",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white10,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
